08d256bca06b39fa0d0508e4f7a47e2d56a411da
maphnew <zcm3118@gmail.com>
2020-01-23 22:18:14 +0900

    26. Creating a React Component

--- README.md
+++ README.md
@@ -765,6 +765,75 @@ console.log(other.getGreeting())
 26. Creating a React Component
 15분
 
+- UpperCase
+- render()
+- \<Component />
+
+```JavaScript
+// src/app.js
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a Computer</h2>
+            </div>
+        )
+    }
+}
+
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        )
+    }
+}
+
+// Challenge
+// Options  -> Options component here
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>option1</li>
+                    <li>option2</li>
+                    <li>option3</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+// Challenge
+// AddOption -> AddOption component here
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <input type="text" />
+                <button>ADD</button>
+            </div>
+        )
+    }
+}
+
+const jsx = (
+    <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+    </div>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
+```
+
 27. Nesting Components
 6분
 
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,75 +1,60 @@
-console.log('App.js is running!')
-
-// if statements
-// ternary operators
-// logical and operator
-
-// only render the subtitle (and p tag) if subtitle exist - logical and operator
-// render new p tag - if options.length > 0 "Here are your options" "No options"
-
-// JSX - JavaScript XML
-
-const app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: []
-}
-
-const onFormSubmit = (e) => {
-    e.preventDefault()
-
-    const option = e.target.elements.option.value
-
-    if (option) {
-        app.options.push(option)
-        e.target.elements.option.value = ''
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Indecision</h1>
+                <h2>Put your life in the hands of a Computer</h2>
+            </div>
+        )
     }
-    render()
 }
 
-const onRemoveAll = () => {
-    app.options = []
-    render()
+class Action extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>What should I do?</button>
+            </div>
+        )
+    }
 }
 
-const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length)
-    const option = app.options[randomNum]
-    alert(option)
+// Challenge
+// Options  -> Options component here
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li>option1</li>
+                    <li>option2</li>
+                    <li>option3</li>
+                </ul>
+            </div>
+        )
+    }
 }
 
-const appRoot = document.getElementById('app')
-
-const numbers = [55, 101, 1000]
-
-const render = () =>{
-    const template = (
-        <div>
-            <h1>{app.title}</h1>
-            {app.subtitle && <p>{app.subtitle}</p>}
-            <p>{(app.options && app.options.length > 0) ? "Here are your options" : "No options"}</p>
-            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
-            <button onClick={onRemoveAll}>Remove All</button>
-            {
-                numbers.map((number)=>{
-                    return <p key={number}>Number: {number}</p>
-                })
-            }
-            {<p>1</p>}
-            <ol>
-                {/* map over app.options getting back an array of lis (set key and text) */
-                    app.options.map((option) => <li key={option}>{option}</li>)
-                }
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option"/>
-                <button>Add Option</button>
-            </form>
-        </div>
-    )
-
-    ReactDOM.render(template, appRoot)
-    
+// Challenge
+// AddOption -> AddOption component here
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <input type="text" />
+                <button>ADD</button>
+            </div>
+        )
+    }
 }
 
-render()
+const jsx = (
+    <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+    </div>
+)
+
+ReactDOM.render(jsx, document.getElementById('app'))
